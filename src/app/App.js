6b7460fbee76a9daa05c903fb6246af8992af26a
Charles Young <cbyoung415@gmail.com>
2023-01-09 09:45:32 -0800

    setup inital idea for routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import React from "react";
+import { Route, Routes } from "react-router";
+import Header from "../components/Header/Header";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" />
+        <Route path="search" />
+        <Route path="r">
+          <Route path=":subreddit" />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
