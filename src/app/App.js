3ff7dd7fdf565a91563f5e3c6074b62506c96d74
Charles Young <cbyoung415@gmail.com>
2023-01-11 10:44:26 -0800

    set route for "/"

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import { CssBaseline, Box, Container } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import Header from "../components/Header/Header";
 import { createAppTheme } from "../util/appTheme";
+import PopularPosts from "../routes/PopularPost/PopularPost";
 
 const App = () => {
   const [mode, setMode] = useState("light");
@@ -19,13 +20,13 @@ const App = () => {
         <Container maxWidth="" disableGutters={true}>
           <Box sx={{ height: "100vh" }}>
             <Header toggleMode={toggleMode} mode={mode} />
-            {/* <Routes>
-              <Route path="/" />
-              <Route path="search" />
+            <Routes>
+              <Route path="/" element={<PopularPosts/>}/>
+              {/* <Route path="search" />
               <Route path="r">
                 <Route path=":subreddit" />
-              </Route>
-            </Routes> */}
+              </Route> */}
+            </Routes>
           </Box>
         </Container>
       </ThemeProvider>
