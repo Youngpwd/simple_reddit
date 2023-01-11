91726c71589e7da75d1b98f6a884381482ddbe60
Charles Young <cbyoung415@gmail.com>
2023-01-11 08:03:36 -0800

    connect store to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import { Provider } from "react-redux";
-// import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
@@ -12,9 +12,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
