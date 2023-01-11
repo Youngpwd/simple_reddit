6c785e8e101cf06b87bddde2df367a7fd1c62e52
Charles Young <cbyoung415@gmail.com>
2023-01-11 07:22:41 -0800

    set custom theme and light/dark toggle

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import { Route, Routes } from "react-router";
+import { CssBaseline, Box, Container } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Header from "../components/Header/Header";
+import { createAppTheme } from "../util/appTheme";
 
 const App = () => {
+  const [mode, setMode] = useState("light");
+  
+  const toggleMode = () => {
+    setMode(mode === "light" ? "dark" : "light");
+  };
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" />
-        <Route path="search" />
-        <Route path="r">
-          <Route path=":subreddit" />
-        </Route>
-      </Routes>
+      <ThemeProvider theme={createAppTheme(mode)}>
+        <CssBaseline />
+        <Container maxWidth="" disableGutters={true}>
+          <Box sx={{ height: "100vh" }}>
+            <Header toggleMode={toggleMode} />
+            {/* <Routes>
+              <Route path="/" />
+              <Route path="search" />
+              <Route path="r">
+                <Route path=":subreddit" />
+              </Route>
+            </Routes> */}
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
