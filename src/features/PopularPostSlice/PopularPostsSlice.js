6f58d7ff9a5626df4ade21002c912f8a5cac7ce6
Charles Young <cbyoung415@gmail.com>
2023-01-11 10:46:25 -0800

    create initial PopularPostSlice that fetches post

---
+++ src/features/PopularPostSlice/PopularPostsSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  posts: [],
+  currentSort: "hot",
+  status: "idle",
+  error: null,
+};
+
+export const fetchPopularPosts = createAsyncThunk(
+  "popularPosts/fetchPopularPosts",
+  async (sortType) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const result = await axios.get(
+      `https://www.reddit.com/r/popular/${sortType}.json`
+    );
+    console.log(result);
+    return result.data.data.children.map((post) => post.data);
+  }
+);
+
+export const popularPostsSlice = createSlice({
+  name: "popularPosts",
+  initialState,
+  reducers: {
+    setCurrentSort: (state, action) => {
+      state.currentSort = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPopularPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPopularPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(fetchPopularPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setCurrentSort } = popularPostsSlice.actions;
+export const selectCurrentSort = (state) => state.popularPosts.currentSort;
+export const selectPosts = (state) => state.popularPosts.posts;
+export const selectLoadingStatus = (state) =>
+  state.popularPosts.status === "loading";
+export const selectError = (state) => state.popularPosts.error;
+
+export default popularPostsSlice.reducer;
