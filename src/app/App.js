48c8d07c2d58b20ef2f46844b5b42f3215f71c5f
Charles Young <cbyoung415@gmail.com>
2023-01-16 09:03:54 -0800

    rearranged components, added scroll component

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import Header from "../components/Header/Header";
 import { createAppTheme } from "../util/appTheme";
 import PopularPosts from "../routes/PopularPost/PopularPost";
+import ScrollToTopButton from "../components/ScrollToTopButton/ScrollToTopButton";
 
 const App = () => {
   const [mode, setMode] = useState("light");
@@ -17,17 +18,20 @@ const App = () => {
     <>
       <ThemeProvider theme={createAppTheme(mode)}>
         <CssBaseline />
-        <Container maxWidth="" disableGutters={true}>
-          <Box sx={{ height: "100vh" }}>
-            <Header toggleMode={toggleMode} mode={mode} />
+        <Container maxWidth="lg" disableGutters={true}>
+          <Header toggleMode={toggleMode} mode={mode} />
+
+          <Box>
             <Routes>
-              <Route path="/" element={<PopularPosts/>}/>
+              <Route path="/" element={<PopularPosts />} />
               {/* <Route path="search" />
               <Route path="r">
                 <Route path=":subreddit" />
               </Route> */}
             </Routes>
           </Box>
+
+          <ScrollToTopButton />
         </Container>
       </ThemeProvider>
     </>
