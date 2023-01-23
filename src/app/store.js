4154cdfef152d434890fc603b519723e3891f7e6
Charles Young <cbyoung415@gmail.com>
2023-01-23 13:07:57 -0800

    add subredditReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import popularPostsReducer from "../features/PopularPostSlice/PopularPostsSlice";
+import subredditReducer from "../features/SubredditSlice/SubredditSlice";
 
 export const store = configureStore({
   reducer: {
     popularPosts: popularPostsReducer,
+    subreddit: subredditReducer,
   },
 });
