9203e2a7187ccf40ced63a645714c5ae9e323094
Charles Young <cbyoung415@gmail.com>
2023-01-25 15:53:54 -0800

    add a useMediaQuery for breakpoints

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router";
-import { CssBaseline, Box, Container } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, Box, Container, useMediaQuery } from "@mui/material";
+import { ThemeProvider, useTheme } from "@mui/material/styles";
 import Header from "../components/Header/Header";
 import { createAppTheme } from "../util/appTheme";
 import PopularPosts from "../routes/PopularPost/PopularPosts";
 import ScrollToTopButton from "../components/ScrollToTopButton/ScrollToTopButton";
 import Subreddit from "../routes/Subreddit/Subreddit";
+
 import "../App.css";
 
 const App = () => {
   const [mode, setMode] = useState("light");
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   const toggleMode = () => {
     setMode(mode === "light" ? "dark" : "light");
@@ -20,14 +23,14 @@ const App = () => {
     <>
       <ThemeProvider theme={createAppTheme(mode)}>
         <CssBaseline />
-        <Container maxWidth="lg" disableGutters={true}>
-          <Header toggleMode={toggleMode} mode={mode} />
+        <Container maxWidth={false} disableGutters={true}>
+          <Header toggleMode={toggleMode} mode={mode} matches={matches}/>
 
           <Box>
             <Routes>
               <Route path="/" element={<PopularPosts />} />
               <Route path="/r">
-                <Route path=":subreddit" element={<Subreddit />} />
+                <Route path=":subreddit" element={<Subreddit matches={matches} />} />
               </Route>
             </Routes>
           </Box>
