07b8328dc3901e0e6f7a78f5a87fd10482670f84
Charles Young <cbyoung415@gmail.com>
2023-01-25 16:03:31 -0800

    create a function to format numbers with a k or m

---
+++ src/util/formatNumber.js
@@ -0,0 +1,9 @@
+export function formatNumber(num) {
+  if (num >= 1000000) {
+    return `${(num / 1000000).toFixed(1)}m`;
+  } else if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}k`;
+  } else {
+    return num;
+  }
+}
