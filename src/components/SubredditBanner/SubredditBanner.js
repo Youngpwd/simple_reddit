1803777ffae2a3aeefc8f15037dfc7820cd36a9b
Charles Young <cbyoung415@gmail.com>
2023-01-27 11:40:22 -0800

    created a new way to handle banner rendering

--- src/components/SubredditBanner/SubredditBanner.js
+++ src/components/SubredditBanner/SubredditBanner.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CardMedia } from "@mui/material";
 import { useSelector } from "react-redux";
 import { selectSubredditAbout } from "../../features/SubredditSlice/SubredditSlice";
 
 const SubredditBanner = ({ matches }) => {
   const about = useSelector(selectSubredditAbout);
+  const [bannerOn, setBannerOn] = useState(true);
+  const [bannerImg, setBannerImg] = useState("");
+
   const imageBanner = () => {
     return about.banner_background_image?.length > 0
-      ? about.banner_background_image.split("?")[0]
+      ? setBannerImg(about.banner_background_image.split("?")[0])
       : about.mobile_banner_image !== ""
-      ? about.mobile_banner_image
-      : about.header_img !== ""
-      ? about.header_img
-      : null;
+      ? setBannerImg(about.mobile_banner_image)
+      : setBannerOn(false);
   };
 
+  useEffect(() => {
+    imageBanner();
+
+    return () => {
+      setBannerImg("");
+    };
+  });
+
   return (
-    <CardMedia
-      component="img"
-      image={imageBanner()}
-      sx={{
-        width: "100%",
-        height: matches ? "200px" : "auto",
-      }}
-    />
+    <>
+      {bannerOn ? (
+        <CardMedia
+          component="img"
+          image={bannerImg}
+          sx={{
+            width: "100%",
+            height: matches ? "200px" : "auto",
+          }}
+        />
+      ) : null}
+    </>
   );
 };
 
