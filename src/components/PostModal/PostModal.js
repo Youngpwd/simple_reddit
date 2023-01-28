2d28e1b14b9c7f4b19a99a13d2531a998d3d9fdf
Charles Young <cbyoung415@gmail.com>
2023-01-27 16:24:30 -0800

    Create a basic modal that can handle modal opening and closing though the store

--- src/components/PostModal/PostModal.js
+++ src/components/PostModal/PostModal.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Box, Modal } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectPost,
+  setPost,
+  setPostOpen,
+} from "../../features/PostSlice/PostSlice";
 
-const PostModal = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const PostModal = ({ open }) => {
+  const dispatch = useDispatch();
+
+  const post = useSelector(selectPost);
+
+  const handleClose = () => {
+    dispatch(setPostOpen(false));
+    dispatch(setPost({}));
+  };
+
   return (
-    <></>
-  )
-}
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={style}>
+        <div>{post.title}</div>
+      </Box>
+    </Modal>
+  );
+};
 
 export default PostModal;
