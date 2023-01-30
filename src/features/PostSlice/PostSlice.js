4e59d044bd6e03bf98b49e87a7456f2294157dec
Charles Young <cbyoung415@gmail.com>
2023-01-30 09:55:52 -0800

    add a fetchComments thunk and selector

--- src/features/PostSlice/PostSlice.js
+++ src/features/PostSlice/PostSlice.js
@@ -1,9 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   postInfo: {},
   postOpen: false,
   comments: [],
+  status: "idle",
+  error: null,
 };
 
 const postSlice = createSlice({
@@ -17,10 +20,41 @@ const postSlice = createSlice({
       state.postOpen = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchComments.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.comments = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
+
+export const fetchComments = createAsyncThunk(
+  "post/fetchComments",
+  async (permalink) => {
+    let commentResults;
+    try {
+      commentResults = await axios.get(
+        `https://www.reddit.com${permalink}.json`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(commentResults.data[1].data.children);
+    return commentResults.data[1].data.children.map((comment) => comment.data);
+  }
+);
 
 export const { setPost, setPostOpen } = postSlice.actions;
 export const selectPost = (state) => state.post.postInfo;
 export const selectPostOpen = (state) => state.post.postOpen;
+export const selectComments = (state) => state.post.comments;
 
 export default postSlice.reducer;
