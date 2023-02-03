4e773fdfb315be5083cdc35a773cde92764da794
Charles Young <cbyoung415@gmail.com>
2023-02-03 09:52:19 -0800

    update

--- src/util/formatTime.js
+++ src/util/formatTime.js
@@ -1,6 +1,8 @@
 import { formatDistance } from "date-fns";
 
 export const formattedTime = (utc) => {
-  const createdAt = utc * 1000;
-  return formatDistance(new Date(createdAt), new Date(), { addSuffix: true });
+  // console.log(utc); //1675371571
+  const createdAt = new Date(utc * 1000);
+  // console.log(createdAt); //Fri Feb 03 2023 05:30:37 GMT-0800 (Pacific Standard Time)
+  return formatDistance(createdAt, new Date(), { addSuffix: true }); //about 3 hours ago
 };
