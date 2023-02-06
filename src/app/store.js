a84b68b77015f62ce3b576513ce8d500b87a9a62
Charles Young <cbyoung415@gmail.com>
2023-02-06 10:47:12 -0800

    add searchReducer to store.

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import popularPostsReducer from "../features/PopularPostSlice/PopularPostsSlice";
 import subredditReducer from "../features/SubredditSlice/SubredditSlice";
 import postReducer from "../features/PostSlice/PostSlice";
+import searchReducer from "../features/search/searchSlice";
 
 export const store = configureStore({
   reducer: {
     popularPosts: popularPostsReducer,
     subreddit: subredditReducer,
     post: postReducer,
+    search: searchReducer
   },
 });
