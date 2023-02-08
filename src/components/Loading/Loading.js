4b981be43231c4082651b90d2f3f786f2ad6c0b9
Charles Young <cbyoung415@gmail.com>
2023-02-08 11:04:42 -0800

    changed skeleton style

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,14 +5,19 @@ import { Box } from "@mui/material";
 
 const Loading = () => {
   return (
-    <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height:"60vh", margin: "0 auto"}}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60vh",
+        margin: "0 auto",
+      }}
+    >
       <Stack spacing={1}>
-        {/* For variant="text", adjust the height via font-size */}
         <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
-        {/* For other variants, adjust the size with `width` and `height` */}
-        <Skeleton variant="circular" width={40} height={40} />
-        <Skeleton variant="rectangular" width={210} height={60} />
-        <Skeleton variant="rounded" width={210} height={60} />
+        <Skeleton variant="rectangular" width={300} height={210} />
+        <Skeleton variant="rounded" width={300} height={50} />
       </Stack>
     </Box>
   );
