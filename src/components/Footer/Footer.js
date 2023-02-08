197525d188f99735bb50f16caa3b1156d1f16f08
Charles Young <cbyoung415@gmail.com>
2023-02-08 10:34:11 -0800

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,57 @@
 import React from "react";
-import { Link, Typography, Container } from "@mui/material";
+import { Link, Typography, Container, Card } from "@mui/material";
 
-const Footer = () => {
+const Footer = ({ matches }) => {
   return (
-    <Container maxWidth="" disableGutters={true} sx={{marginTop: 5, display: "flex", justifyContent: "space-evenly"}} >
-      <Typography variant="body2">
-        Data displayed on this website does not belong to me and is used for
-        educational purposes only.
-      </Typography>
-      <Link href="https://github.com/Youngpwd/simple_reddit" target="_blank" color="inherit">
-        https://github.com/Youngpwd/simple_reddit
-      </Link>
+    <Container
+      maxWidth=""
+      disableGutters={true}
+      sx={{
+        padding: ".5rem",
+        margin: ".7rem 0 0 0",
+      }}
+    >
+      <Card
+        raised={true}
+        sx={
+          matches
+            ? {
+                margin: "1rem auto 0",
+                display: "flex",
+                justifyContent: "space-evenly",
+                width: "100%",
+              }
+            : {
+                marginTop: 5,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+              }
+        }
+      >
+        <Typography variant="caption" p={3}>
+          Data displayed on this website does not belong to me and is used for
+          educational purposes only.
+        </Typography>
+        <Link
+          href="https://github.com/Youngpwd/simple_reddit"
+          target="_blank"
+          color="inherit"
+          variant="caption"
+          sx={
+            matches
+              ? {
+                  "&:hover": { textDecorationColor: "red", fontSize: "2rem" },
+                }
+              : null
+          }
+          p={3}
+        >
+          https://github.com/Youngpwd/simple_reddit
+        </Link>
+      </Card>
     </Container>
   );
 };
